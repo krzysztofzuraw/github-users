c8260537f2660c76b2a12a316d7fb1486a055702
Krzysztof Żuraw <krzysztofzuraw@fastmail.com>
2018-06-15 20:42:01 +0200

    Add basic layout

--- src/components/main-card.tsx
+++ src/components/main-card.tsx
@@ -1,7 +1,30 @@
-import { Card } from "antd";
+import { Avatar, Button, Card, List } from "antd";
 import * as React from "react";
-import { style } from "typestyle";
+import { FormattedMessage } from "react-intl";
 
 export const MainCard: React.SFC = () => {
-  return <Card>Something</Card>;
+  return (
+    <Card title={<FormattedMessage id="main.card.header" defaultMessage="Who to follow" />}>
+      <List
+        itemLayout="horizontal"
+        dataSource={["1", "2", "3"]}
+        loadMore={
+          <div>
+            <Button>Refresh</Button>
+          </div>
+        }
+        renderItem={(item: any) => (
+          <List.Item actions={[<a>follow</a>]}>
+            <List.Item.Meta
+              avatar={
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              }
+              title="User Name"
+              description="User description goes here"
+            />
+          </List.Item>
+        )}
+      />
+    </Card>
+  );
 };
